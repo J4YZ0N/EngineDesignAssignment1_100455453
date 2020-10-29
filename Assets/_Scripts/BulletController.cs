d32aeda91732b7746809ec589804964cf6e7bd25
Jayson MacFarlane <jayson.macfarlane13@gmail.com>
2020-10-28 22:52:55 -0400

    Initial commit with content

---
+++ Assets/_Scripts/BulletController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Util;
+
+public class BulletController : MonoBehaviour
+{
+    public float bulletSpeed = 0.1f;
+    public Boundary boundary;
+
+    //a reference to the BulletPoolManager
+    //private BulletPoolManager _manager;
+
+    void Start()
+    {
+        boundary.Top = 2.45f;
+        //_manager = FindObjectOfType<BulletPoolManager>();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        CheckBounds();
+    }
+
+    private void Move()
+    {
+        transform.position += new Vector3(0.0f, bulletSpeed * Time.deltaTime, 0.0f);
+    }
+
+    private void CheckBounds()
+    {
+        if (transform.position.y >= boundary.Top)
+        {
+            // Returns the bullet to the pool
+            BulletPoolManager.instance.ResetBullet(this.gameObject);
+        }
+    }
+}
---
+++ Assets/_Scripts/BulletPoolManager.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class BulletPoolManager : MonoBehaviour
+{
+    // Reference to BulletPoolManager to turn into singleton
+    public static BulletPoolManager instance = null;
+
+    public GameObject bullet;
+
+    public int maxBullets;
+
+    // Structure to contain a collection of bullets
+    private Queue<GameObject> bulletList;
+
+    // Turns an instance of this script into a singleton for public use
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bulletList = new Queue<GameObject>();
+        BuildBulletPool();
+    }
+
+    // Adds a series of bullets to the bullet pool
+    private void BuildBulletPool()
+    {
+        for (int i = 0; i < maxBullets; i++)
+        {
+            GameObject newBullet = Instantiate(bullet, this.transform);
+            bulletList.Enqueue(newBullet);
+            newBullet.SetActive(false);
+        }
+    }
+
+    // Returns current count of the bullet pool
+    public int GetBulletPoolSize()
+    {
+        return bulletList.Count;
+    }
+
+    // Calls GetBulletPoolSize() to check if it is empty or not
+    public bool IsPoolEmpty()
+    {
+        if(GetBulletPoolSize() == 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    // Gets a bullet from the pool and activates it
+    // If pool is empty it runs BuildBulletPool() again to create more bullets
+    public GameObject GetBullet()
+    {
+        if (IsPoolEmpty())
+        {
+            BuildBulletPool();
+            bullet = bulletList.Dequeue();
+            bullet.SetActive(true);
+            return bullet;
+        }
+        else
+        {
+            bullet = bulletList.Dequeue();
+            bullet.SetActive(true);
+            return bullet;
+        }
+    }
+
+    // Returns a bullet back to the pool and deactivates it
+    public void ResetBullet(GameObject bullet)
+    {
+        bulletList.Enqueue(bullet);
+        bullet.SetActive(false);
+    }
+}
